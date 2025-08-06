5abb77fab2053379db63078368727f3e8c679569
Taulant Sela <taulant1995@gmail.com>
2025-08-06 16:26:22 +0200

    remove new line

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ function App() {
   return (
     <div className="min-h-screen bg-black text-white flex items-center justify-center">
       <h1 className="text-3xl font-bold">Tailwind is finally working 🎉</h1>
-      <p>is it though ?</p>
     </div>
   );
 }
